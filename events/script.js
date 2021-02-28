856980a4cb2fe40abb94534c8f62be1a83f2b4a4
Daiki-Kyoto <d.k811bb@gmail.com>
2021-02-28 15:43:02 +0900

    changed

--- events/script.js
+++ events/script.js
@@ -1,12 +1,88 @@
 $(function() {
-  // ヘッダーのナビゲーションを一定スクロールで表示
-  var hList = $('.h-list');
 
   $(window).scroll(function() {
-    if ($(this).scrollTop() > 340 && $(window).width() > 500) {
-      hList.fadeIn();
+    document.getElementById('count').innerText = $(this).scrollTop();
+  });
+
+  $('.menu-icon').click(function() {
+    $('#sidemenu').toggle();
+  });
+
+
+  // ヘッダーのナビゲーションを一定スクロールで表示
+  var $win = $(window)
+
+  var menubar = $('#menubar');
+  $win.scroll(function() {
+    if ($(this).scrollTop() > 200 && $win.width() > 600) {
+      menubar.fadeIn();
     } else {
-      hList.fadeOut();
+      menubar.fadeOut();
+    }
+  });
+  
+  
+  var bars = $('#bars')
+  $win.scroll(function() {
+    if ($(this).scrollTop() > 200 && $win.width() <= 600) {
+      bars.fadeIn();
+    } else {
+      bars.fadeOut();
+    }
+  });
+
+  // ミッションを一定スクロールで表示
+  var $mis = $('.mission');
+  $win.scroll(function() {
+    if ($win.width() > 500) {
+      if ($(this).scrollTop() > 0) {
+        $mis.fadeIn();
+      }
+    } else {
+      if ($(this).scrollTop() > 100) {
+        $mis.fadeIn();
+      }
+    }
+  });
+
+  var $band = $('.band-image img');
+  $win.scroll(function() {
+    if ($win.width() > 500) {
+      if ($(this).scrollTop() > 250) {
+        $band.fadeIn();
+      }
+    } else {
+      if ($(this).scrollTop() > 540) {
+        $band.fadeIn();
+      }
+    }
+  });
+
+  // メッセージを一定スクロールで表示
+  var $mes = $('.messages');
+  $win.scroll(function() {
+    if ($win.width() > 500) {
+      if ($(this).scrollTop() > 500) {
+        $mes.fadeIn();
+      }
+    } else {
+      if ($(this).scrollTop() > 700) {
+        $mes.fadeIn();
+      }
+    }
+  });
+
+
+  var $vmtg = $('.visit-mtg');
+  $win.scroll(function() {
+    if ($win.width() > 500) {
+      if ($(this).scrollTop() > 1000) {
+        $vmtg.fadeIn();
+      }
+    } else {
+      if ($(this).scrollTop() > 1300) {
+        $vmtg.fadeIn();
+      }
     }
   });
 
