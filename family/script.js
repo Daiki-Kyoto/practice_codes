f2e444e799071d1cacfdd630bddf11cdfd86f051
Daiki-Kyoto <d.k811bb@gmail.com>
2021-03-02 16:57:07 +0900

    edited family page

--- family/script.js
+++ family/script.js
@@ -1,36 +1,92 @@
 $(function() {
+
+  $(window).scroll(function() {
+    document.getElementById('count').innerText = $(this).scrollTop();
+  });
+
+  $('.menu-icon').click(function() {
+    $('#sidemenu').toggle();
+  });
+
+
   // ヘッダーのナビゲーションを一定スクロールで表示
-  var hList = $('.h-list');
   var $win = $(window)
+
+  var menubar = $('#menubar');
   $win.scroll(function() {
-    if ($(this).scrollTop() > 340 && $(window).width() > 500) {
-      hList.fadeIn();
+    if ($(this).scrollTop() > 200 && $win.width() > 600) {
+      menubar.fadeIn();
     } else {
-      hList.fadeOut();
+      menubar.fadeOut();
+    }
+  });
+  
+  
+  var bars = $('#bars')
+  $win.scroll(function() {
+    if ($(this).scrollTop() > 200 && $win.width() <= 600) {
+      bars.fadeIn();
+    } else {
+      bars.fadeOut();
     }
   });
 
-  $('.toTopPage').click(function(){
-    $('html, body').animate({
-      'scrollTop':0
-    }, 500);
-  });
+  // ミッションを一定スクロールで表示
+  // var $mis = $('.mission');
+  // $win.scroll(function() {
+  //   if ($win.width() > 500) {
+  //     if ($(this).scrollTop() > 0) {
+  //       $mis.fadeIn();
+  //     }
+  //   } else {
+  //     if ($(this).scrollTop() > 100) {
+  //       $mis.fadeIn();
+  //     }
+  //   }
+  // });
 
-  $win.scroll(function() {
-    if($(this).scrollTop() > 0) {
-      $('.toTopPage').fadeIn();
-    } else {
-      $('.toTopPage').fadeOut();
-    }
-  });
+  // var $band = $('.band-image img');
+  // $win.scroll(function() {
+  //   if ($win.width() > 500) {
+  //     if ($(this).scrollTop() > 250) {
+  //       $band.fadeIn();
+  //     }
+  //   } else {
+  //     if ($(this).scrollTop() > 540) {
+  //       $band.fadeIn();
+  //     }
+  //   }
+  // });
 
-  $win.scroll(function() {
-    if($(this).scrollTop() > 340) {
-      $('.sidebanner').addClass('is-fixed');
-    } else {
-      $('.sidebanner').removeClass('is-fixed');
-    }
-  });
+  
+
+  // メッセージを一定スクロールで表示
+  // var $mes = $('.messages');
+  // $win.scroll(function() {
+  //   if ($win.width() > 500) {
+  //     if ($(this).scrollTop() > 500) {
+  //       $mes.fadeIn();
+  //     }
+  //   } else {
+  //     if ($(this).scrollTop() > 700) {
+  //       $mes.fadeIn();
+  //     }
+  //   }
+  // });
+
+
+  // var $vmtg = $('.visit-mtg');
+  // $win.scroll(function() {
+  //   if ($win.width() > 500) {
+  //     if ($(this).scrollTop() > 1000) {
+  //       $vmtg.fadeIn();
+  //     }
+  //   } else {
+  //     if ($(this).scrollTop() > 1300) {
+  //       $vmtg.fadeIn();
+  //     }
+  //   }
+  // });
 
 
 });
